d1f9302bd4ace4891bdc4683445b8d8b18db86c7
Dmytro Holub <heap3d@gmail.com>
2024-01-14 23:45:17 +0200

    formatted cars/models.py

--- cars/models.py
+++ cars/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Driver(models.Model):
     name = models.TextField()
     license = models.TextField()
+
 
 class Car(models.Model):
     make = models.TextField()
--- postgresTest/settings.py
+++ postgresTest/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'testdb',
-    'cars',
+    'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
